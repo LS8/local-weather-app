05cd248f08b19de9009ca771c64716d603168cb3
schoettker <lennartschoettker@hotmail.com>
2016-12-08 10:05:54 +0100

    code clean up : more descriptive function names, less usage of var, no global variables

--- script.js
+++ script.js
@@ -1,47 +1,32 @@
-var output = document.getElementById('output');
-var latitude, longitude, weatherData;
+window.onload = function() {
+  requestWeatherData();
+}
 
-function getCoords() {
+function requestWeatherData() {
   if (!navigator.geolocation)
-    console.log('Geolocation is not supported');
-
-  function succes(position) {
-    latitude = position.coords.latitude, longitude = position.coords.longitude; 
-    getWeather(latitude, longitude);
+    console.log('Your browser does not support GeoLocation');
+  function success(position) {
+    var latitude = position.coords.latitude, longitude = position.coords.longitude; 
+    XHRequest(latitude, longitude);
   }
-
   function error() {
     console.log('Error while retrieving location');
   }
+  navigator.geolocation.getCurrentPosition(success, error);
+}
 
-  navigator.geolocation.getCurrentPosition(succes, error);
-}
-getCoords();
-//make this on load
-function listener(response) {
-  weatherData = JSON.parse(response);
-  var city = weatherData.name, temp = weatherData.main.temp, desc = weatherData.weather[0].description, wind = weatherData.wind.speed;
-  var weatherInfo = [];
-  weatherInfo.push(city + ', ' + weatherData.sys.country, desc, temp  + ' ' + String.fromCharCode(176) + 'F', wind + ' mph');
-  getIcon('icon', weatherData.weather[0].id, 'wi-owm-');
-  getIcon('temp', 'wi-thermometer');
-  getIcon('wind', 'wi-small-craft-advisory');
-  getDate('date');
-  appendToDOM(weatherInfo);
-  toggleUnit(temp);
-}
-function getWeather(lat, lon) {
+function XHRequest(lat, lon) {
   var cors = 'https://crossorigin.me/';
   var key = 'c6ab22fb510471242d6784203ec9a3ff';
-  // var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial' + '&APPID=' + key;
-  var url = cors + 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial' + '&APPID=' + key;
+  // Use this url for local usage
+  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial' + '&APPID=' + key;
+  // and this for https (e.g github pages)
+  // var url = cors + 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial' + '&APPID=' + key;
   var request = new XMLHttpRequest();
-  //get rid of this
-  // request.addEventListener('load', listener);
   request.open('GET', url);
   request.onload = function() {
     if (this.status >= 200 && this.status < 400) {
-      listener(this.responseText);
+      handleResponse(this.responseText);
     } else {
       console.log("Server returned an Error#", this.status);
     }
@@ -52,10 +37,23 @@ function getWeather(lat, lon) {
   request.send();
 }
 
+function handleResponse(response) {
+  weatherData = JSON.parse(response);
+  var city = weatherData.name, temp = weatherData.main.temp, desc = weatherData.weather[0].description, wind = weatherData.wind.speed,
+    weatherInfo = [];
+  weatherInfo.push(city + ', ' + weatherData.sys.country, desc, temp  + ' ' + String.fromCharCode(176) + 'F', wind + ' mph');
+  getIcon('icon', weatherData.weather[0].id, 'wi-owm-');
+  getIcon('temp', 'wi-thermometer');
+  getIcon('wind', 'wi-small-craft-advisory');
+  getDate('date');
+  appendToDOM(weatherInfo);
+  toggleUnit(temp);
+}
+
 function appendToDOM(arr) {
   for (var i = 1, l = arr.length; i <= l; i++) {
-    var element = document.createTextNode(arr[i-1]);
-    var parent = document.getElementById('weather-' + i);
+    var element = document.createTextNode(arr[i-1]),
+      parent = document.getElementById('weather-' + i);
     parent.appendChild(element);
   }
 }
@@ -67,13 +65,13 @@ function getIcon(id, code, ref = false) {
     icon.className += ' ' + code;
 }
 function getDate(id) {
-  var d = new Date();
-  document.getElementById(id).innerHTML = d.toDateString();
+  var date = new Date();
+  document.getElementById(id).innerHTML = date.toDateString();
 }
 
 function toggleUnit(temp) {
-  var unit = document.getElementById('weather-3');
-  var fahrenheit = true;
+  var unit = document.getElementById('weather-3'),
+    fahrenheit = true;
   unit.addEventListener('click', function(event) {
     var toChange = this.childNodes[1];
     if (fahrenheit) {
@@ -83,5 +81,5 @@ function toggleUnit(temp) {
       toChange.nodeValue = temp + ' ' + String.fromCharCode(176) + 'F';
       fahrenheit = true;
     }
-  })
+  });
 }
