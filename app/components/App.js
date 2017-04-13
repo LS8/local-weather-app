318db1a57dead81f390299639a01bb694ebb068a
LS8 <lennartschoettker@hotmail.com>
2017-04-13 20:08:31 +0200

    add Switch

--- app/components/App.js
+++ app/components/App.js
@@ -2,6 +2,7 @@ const React = require('react');
 const ReactRouter = require('react-router-dom');
 const Router = ReactRouter.BrowserRouter;
 const Route = ReactRouter.Route;
+const Switch = ReactRouter.Switch;
 const Hello = require('./Hello');
 
 class App extends React.Component {
@@ -9,10 +10,12 @@ class App extends React.Component {
     return (
       <Router>
         <div className='container'>
-          <Route exact path='/' component={Hello}/>
-          <Route render={function() {
-            return <p>Not Found</p>
-          }}/>
+          <Switch>
+            <Route exact path='/' component={Hello}/>
+            <Route render={function() {
+              return <p>Not Found</p>
+            }}/>
+          </Switch>
         </div>
       </Router>
     )
